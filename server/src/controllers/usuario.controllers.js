48c7dff1ce81da4d295b132abab09903dbaa413a
Jorman Noriega <jormandavidnoriegadavila@gmail.com>
2024-04-30 01:05:19 -0500

    Controllers Usuario

--- server/src/controllers/usuario.controllers.js
+++ server/src/controllers/usuario.controllers.js
@@ -1,12 +1,27 @@
 import { Usuario } from "../models/Usuario.js";
 
-
 export async function getUsuarios(req, res) {
   try {
     const usuarios = await Usuario.findAll({
       atributes: ["idUsuario", "nombre", "priority", "correo", "contraseña"],
     });
     res.json(usuarios);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+}
+
+export async function getUsuario(req, res) {
+  const { idUsuario } = req.params;
+  try {
+    const usuario = await Usuario.findOne({
+      where: {
+        idUsuario,
+      },
+    });
+    res.json(usuario);
   } catch (error) {
     res.status(500).json({
       message: error.message,
@@ -35,3 +50,35 @@ export async function createUsuario(req, res) {
   }
   res.json("received");
 }
+
+export const updateUsuario = async (req, res) => {
+  try {
+    const { idUsuario } = req.params;
+    const { nombre, correo, contraseña } = req.body;
+
+    const usuario = await Usuario.findByPk(idUsuario);
+    usuario.nombre = nombre;
+    usuario.correo = correo;
+    usuario.contraseña = contraseña;
+    await usuario.save();
+
+    res.json(usuario);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+export const deleteUsuario = async (req, res) => {
+  const { idUsuario } = req.params;
+  try {
+    //vaciar antes las tablas relacionadas
+    await Usuario.destroy({
+      where: {
+        idUsuario,
+      },
+    });
+    return res.sendStatus(204);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
--- server/src/routes/usuario.routes.js
+++ server/src/routes/usuario.routes.js
@@ -1,13 +1,18 @@
 import { Router } from "express";
-import {getUsuarios,createUsuario} from "../controllers/usuario.controllers.js"
+import {
+  getUsuarios,
+  createUsuario,
+  getUsuario,
+  updateUsuario,
+  deleteUsuario,
+} from "../controllers/usuario.controllers.js";
 
 const router = Router();
 
-
-router.get("/usuarios",getUsuarios);
-router.post("/usuarios",createUsuario);
-router.put("/usuarios/:id");
-router.delete("/usuarios/:id");
-router.get("/usuarios/:id");
+router.get("/", getUsuarios);
+router.post("/", createUsuario);
+router.put("/:idUsuario", updateUsuario);
+router.delete("/:idUsuario", deleteUsuario); //toca eliminar antes todo lo q tenga en otras tablas :D
+router.get("/:idUsuario", getUsuario);
 
 export default router;
