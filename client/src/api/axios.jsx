12a27938c2fd6ecad3568e630ef2836234ce64c2
Jorman Noriega <jormandavidnoriegadavila@gmail.com>
2024-10-24 23:01:02 -0500

    accept

--- client/src/api/axios.jsx
+++ client/src/api/axios.jsx
@@ -3,7 +3,8 @@ import axios from "axios";
 const instance = axios.create({
     baseURL: 'https://stockwise-dpp8.onrender.com/api',
     headers: {
-        'Content-Type': 'application/json', 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json', 
       },
     withCredentials: true
 })
