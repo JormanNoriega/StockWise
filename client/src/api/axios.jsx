c888262d755601a7290e09b0258323b6f6d797f5
Jorman Noriega <jormandavidnoriegadavila@gmail.com>
2024-10-24 22:06:22 -0500

    withCredentials

--- client/src/api/axios.jsx
+++ client/src/api/axios.jsx
@@ -3,8 +3,9 @@ import axios from "axios";
 const instance = axios.create({
     baseURL: 'https://stockwise-dpp8.onrender.com/api',
     headers: {
-        'Content-Type': 'application/json',
+        'Content-Type': 'application/json', 
       },
+    withCredentials: true
 })
 
 export default instance;
